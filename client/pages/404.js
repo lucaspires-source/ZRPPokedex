99eac56c4895176319a9dfdd4a4114d591089752
lucaspires-source <pires.lucas94@gmail.com>
2023-05-18 08:25:56 -0300

    adding custom 404 error page

---
+++ client/pages/404.js
@@ -0,0 +1,9 @@
+export default function Custom404() {
+    return  (
+        <div className="pageWrapper">
+            <div className = "notFound-container">
+            <img src="https://i.pinimg.com/originals/05/51/f5/0551f506725ac1deeaa85d46f8b9a5fd.jpg" alt="404"/>
+            </div>
+        </div>
+    )
+  }
